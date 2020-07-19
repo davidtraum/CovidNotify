75917c424fdd27609fc8cf7d2c215f880e8acd91
davidtraum <david@traum.me>
2020-07-19 14:55:21 +0200

    Initial

--- src/covid_core.py
+++ src/covid_core.py
@@ -2,16 +2,24 @@ import requests
 
 class CovidCore:
 
-    def __init__(self, base = 'https://api.covid19api.com/summary'):
+    def __init__(self, base = 'https://api.covid19api.com/summary', debug=False):
         self.data = dict()
         self.base = base
         self.loaded = False
+        self.debug = debug
         self.reloadData()
 
+    def debug(self, msg):
+        print("[CovidCore]", msg)
+
     def reloadData(self):
+        before = int(time.time() * 1000)
         request = requests.get(url = self.base)
         self.data = request.json()
         self.loaded = 'Countries' in self.data
+        if(self.debug):
+            after = int(time.time() * 1000) - before
+            self.debug("Data loaded in " + str(after) + " ms")
 
     def getCountry(self, name):
         if(not self.loaded):
